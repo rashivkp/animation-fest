4d529bd34e47ea59b3e49da06e1fa09e31d032d2
Mohamed Rashid <rashivkp@gmail.com>
2014-07-23 10:10:28 +0530

    adding a basic template adding a jquery-rate plugin

--- fest/urls.py
+++ fest/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
 
-from fest.views import items
+from fest.views import items, rateMe
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
     url(r'^items/$', items, name='items'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^rate/$', rateMe, name='rating'),
 )
 
 if settings.DEBUG and settings.MEDIA_ROOT:
--- fest/views.py
+++ fest/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+import json
+from django.core import serializers
 from fest.models import *
+from django.views.decorators.csrf import csrf_exempt
 
 def items(request):
     items = Item.objects.all()
     return render_to_response('itemlist.html', { 'items': items})
+
+@csrf_exempt
+def rateMe(request):
+    if request.method == 'POST':
+        print request.POST['idStudent']
+        students = Student.objects.all()
+        return HttpResponse(serializers.serialize('json', students ),
+            mimetype='application/json')
 
